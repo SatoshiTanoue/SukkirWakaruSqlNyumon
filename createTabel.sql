0b2be96e2a18b088adeb448e2305c7828e2c185b
Satoshi Tanoue <again2222@gmail.com>
2017-01-24 17:11:35 +0900

    文法が間違っていたので修正

--- createTabel.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE 家計簿 (日付 date(13),費目 varchar(20),メモ varchar(20),入金額 INTEGER(10),出金額 INTEGER(10))
-CREATE TABLE 家計簿アーカイブ (日付 date(13),費目 varchar(20),メモ varchar(20),入金額 INTEGER(10),出金額 INTEGER(10))
-CREATE TABLE 家計簿集計 (費目 varchar(20),合計 INTEGER(10),平均 INTEGER(10),最大 INTEGER(10),最小 INTEGER(10),回数 INTEGER(10))
-
-
-
